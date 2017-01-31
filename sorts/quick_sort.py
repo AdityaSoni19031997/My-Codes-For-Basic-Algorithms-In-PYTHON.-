6daedf71e792490ebaf3f4e71370d474ff2af669
AdityaSoni19031997 <soniaditya922@gmail.com>
2017-01-31 10:46:31 +0530

    Add files via upload

---
+++ sorts/quick_sort.py
@@ -0,0 +1,66 @@
+"""
+This is a pure python implementation of the quick sort algorithm
+
+For doctests run following command:
+python -m doctest -v quick_sort.py
+or
+python3 -m doctest -v quick_sort.py
+
+For manual testing run:
+python quick_sort.py
+"""
+from __future__ import print_function
+from random import shuffle
+
+
+def sort(collection):
+    shuffle(collection)
+    return quick_sort(collection)
+
+
+def quick_sort(collection):
+    """Pure implementation of quick sort algorithm in Python
+
+    :param collection: some mutable ordered collection with heterogeneous
+    comparable items inside
+    :return: the same collection ordered by ascending
+
+    Examples:
+    >>> quick_sort([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+
+    >>> quick_sort([])
+    []
+
+    >>> quick_sort([-2, -5, -45])
+    [-45, -5, -2]
+    """
+    if len(collection) <= 1:
+        return collection
+    less = []
+    equal = []
+    greater = []
+    pivot = collection[0]
+    for x in collection:
+        if x < pivot:
+            less.append(x)
+        elif x == pivot:
+            equal.append(x)
+        else:
+            greater.append(x)
+    return quick_sort(less) + equal + quick_sort(greater)
+
+
+if __name__ == '__main__':
+    import sys
+
+    # For python 2.x and 3.x compatibility: 3.x has not raw_input builtin
+    # otherwise 2.x's input builtin function is too "smart"
+    if sys.version_info.major < 3:
+        input_function = raw_input
+    else:
+        input_function = input
+
+    user_input = input_function('Enter numbers separated by a comma:\n')
+    unsorted = [int(item) for item in user_input.split(',')]
+    print(sort(unsorted))
