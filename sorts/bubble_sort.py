e45e3efa16ab0c7f9e5947aef3db6822fd5fba87
Aditya Soni <soniaditya922@gmail.com>
2017-06-01 16:40:25 +0530

    Update bubble_sort.py

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,11 +1,6 @@
 """
 This is pure python implementation of bubble sort algorithm
 
-For doctests run following command:
-python -m doctest -v bubble_sort.py
-or
-python3 -m doctest -v bubble_sort.py
-
 For manual testing run:
 python bubble_sort.py
 """
@@ -42,7 +37,6 @@ def bubble_sort(collection):
 if __name__ == '__main__':
     import sys
     # For python 2.x and 3.x compatibility: 3.x has not raw_input builtin
-    # otherwise 2.x's input builtin function is too "smart"
     if sys.version_info.major < 3:
         input_function = raw_input
     else:
