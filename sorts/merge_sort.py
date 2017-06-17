d91b69c43e937eb30980cb6353a6457ff717fa9f
Aditya Soni <soniaditya922@gmail.com>
2017-06-17 13:47:30 +0530

    Update merge_sort.py

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -4,67 +4,64 @@ This is a pure python implementation of the merge sort algorithm
 For manual testing run:
 python merge_sort.py
 """
-from __future__ import print_function
+import sys
 
+# 1st routine: split the input array recursively
+# until atomic sub-arrays are reached
+def mergeSort(inArray, first, last):
+    if first < last:
+        middle = (first + last) / 2          # returns floored int
+        mergeSort(inArray, first, middle)
+        mergeSort(inArray, middle + 1, last)
+        merge(inArray, first, middle, last)
 
-def merge_sort(collection):
-    """Pure implementation of the merge sort algorithm in Python
+# 2nd routine: merge individual sub-arrays to one sorted array
+def merge(inArray, first, middle, last):
+    n1 = middle - first + 1 # length of inArray[first,middle]
+    n2 = last - middle      # length of inArray[middle+1,last]
 
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
+    leftArray = []
+    rightArray = []
+    for i in range(n1):
+        leftArray.append(inArray[first+i])
+    for j in range(n2):
+        rightArray.append(inArray[middle+1+j])
 
-    Examples:
-    >>> merge_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
+    # maxint as the sentinel (last value) guarantees 
+    # that both subarrays will be processed completely 
+    leftArray.append(sys.maxint)
+    rightArray.append(sys.maxint)
 
-    >>> merge_sort([])
-    []
-
-    >>> merge_sort([-2, -5, -45])
-    [-45, -5, -2]
-    """
-    length = len(collection)
-    if length > 1:
-        midpoint = length // 2
-        left_half = merge_sort(collection[:midpoint])
-        right_half = merge_sort(collection[midpoint:])
-        i = 0
-        j = 0
-        k = 0
-        left_length = len(left_half)
-        right_length = len(right_half)
-        while i < left_length and j < right_length:
-            if left_half[i] < right_half[j]:
-                collection[k] = left_half[i]
-                i += 1
-            else:
-                collection[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < left_length:
-            collection[k] = left_half[i]
+    i,j = 0,0
+    k = first
+    while k <= last:
+        if leftArray[i] <= rightArray[j]:
+            inArray[k] = leftArray[i]
             i += 1
-            k += 1
-
-        while j < right_length:
-            collection[k] = right_half[j]
+        else:
+            inArray[k] = rightArray[j]
             j += 1
-            k += 1
+        k += 1
 
-    return collection
+# alternative merge implmementation, using
+# python's arrays slicing utilities		
+def merge2(inArray, first, middle, last):
+    leftArray = inArray[first:middle+1]
+    rightArray = inArray[middle+1:last+1]
 
+    # maxint as the last value guarantees that both
+    # subarrays will be processed completely
+    leftArray.append(sys.maxint)
+    rightArray.append(sys.maxint)
 
-if __name__ == '__main__':
-    import sys
-
-    # For python 2.x and 3.x compatibility: 3.x has not raw_input builtin
-    if sys.version_info.major < 3:
-        input_function = raw_input
-    else:
-        input_function = input
-
-    user_input = input_function('Enter numbers separated by a comma:\n')
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(merge_sort(unsorted))
+    i,j = 0,0
+    k = first
+    while k <= last:
+        if leftArray[i] <= rightArray[j]:
+            inArray[k] = leftArray[i]
+            i += 1
+        else:
+            inArray[k] = rightArray[j]
+            j += 1
+        k += 1
+		
